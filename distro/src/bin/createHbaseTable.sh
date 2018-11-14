60c541696f56e333e68f23e9b75a65a198809317
lidanqi <lidanqi@gridsum.com>
2018-11-14 10:48:32 +0800

    ui

--- distro/src/bin/createHbaseTable.sh
+++ distro/src/bin/createHbaseTable.sh
@@ -14,7 +14,7 @@ function createTable {
     echo "Creating the Table <$1>" 
 
     echo "create '$1', $2" | hbase shell > log 2>&1
-    echo " grant 'METASPACE', 'RWCA', 'table_stat'" | hbase shell > log 2>&1
+    echo "grant 'METASPACE', 'RWCA', 'table_stat'" | hbase shell > log 2>&1
 
     cat log | grep -q '0 row(s) in' 
 
