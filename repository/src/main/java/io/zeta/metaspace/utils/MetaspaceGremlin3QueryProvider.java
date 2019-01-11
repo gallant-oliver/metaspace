e3e4e29df8e47c9b90fa1c006a9cf8e76115854a
sunhaoning <sunhaoning@gridsum.com>
2019-01-11 14:53:06 +0800

    查询图数据库添加事务提交

--- repository/src/main/java/io/zeta/metaspace/utils/MetaspaceGremlin3QueryProvider.java
+++ repository/src/main/java/io/zeta/metaspace/utils/MetaspaceGremlin3QueryProvider.java
@@ -59,7 +59,7 @@ public class MetaspaceGremlin3QueryProvider extends MetaspaceGremlinQueryProvide
 
             case TABLE_GUID_QUERY:
                 //g.tx().commit();
-                return "g.V().has('__typeName','hive_db').has('Asset.name','%s').inE().outV().has('__typeName','hive_table').has('Asset.name','%s').values('__guid').toList()";
+                return "g.tx().commit();g.V().has('__typeName','hive_db').has('Asset.name','%s').inE().outV().has('__typeName','hive_table').has('Asset.name','%s').values('__guid').toList()";
 
             case TABLE_DB_BY_QUERY:
                 return "g.V().has('__typeName', 'hive_table').has('Asset.name', org.janusgraph.core.attribute.Text.textRegex('.*%s.*')).has('__guid').order().by('__timestamp').dedup().range(%s, %s).as('table').outE().inV().has('__typeName','hive_db').as('db').select('table','db').toList()";
