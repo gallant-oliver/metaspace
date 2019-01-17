e01150bae680796b6d7a6ddf89cf6b9b802f1a15
lidanqi <lidanqi@gridsum.com>
2019-01-17 16:55:22 +0800

    V2

--- webapp/src/main/java/io/zeta/metaspace/web/dao/DataQualityV2DAO.java
+++ webapp/src/main/java/io/zeta/metaspace/web/dao/DataQualityV2DAO.java
@@ -4,6 +4,7 @@ import io.zeta.metaspace.model.dataquality.Report;
 import io.zeta.metaspace.model.result.ReportResult;
 import io.zeta.metaspace.model.result.TableColumnRules;
 import io.zeta.metaspace.model.result.TemplateResult;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.sql.SQLException;
@@ -11,12 +12,12 @@ import java.util.List;
 
 public interface DataQualityV2DAO {
     //report template
-    @Select("select report.templateid templateid,report.orangealerts, report.redalerts, report.reportid,report.reportname,report.alert,temp.buildtype buildtype,temp.periodcron periodcron,temp.templatename templatename,temp.templatestatus,temp.starttime,temp.tablerulesnum,temp.columnrulesnum,MAX(reportproducedate) OVER (PARTITION BY report.templateid)\n" +
+    @Select("select * from (select reportproducedate,report.templateid templateid,orangealerts, redalerts,reportid,reportname,alert,temp.buildtype buildtype,temp.periodcron periodcron,temp.templatename templatename,templatestatus,starttime,tablerulesnum,columnrulesnum,MAX(reportproducedate) OVER (PARTITION BY report.templateid) maxtime \n" +
             "from\n" +
             "(select templateid,buildtype,periodcron,templatename,templatestatus,starttime,tablerulesnum,columnrulesnum\n" +
             "from template \n" +
             "where tableid = #{tableId}) as temp,report \n" +
-            "where report.templateid=temp.templateid")
+            "where report.templateid=temp.templateid) tere where  reportproducedate=maxtime")
     public List<TemplateResult> getTemplateResults( String tableId) throws SQLException;
     @Select("select reportid,reportname,source,templatename,periodcron,buildtype,reportproducedate,redalerts,orangealerts from report where reportid = #{reportId}")
     public List<Report> getReport(String reportId) throws SQLException;
@@ -24,8 +25,8 @@ public interface DataQualityV2DAO {
     public List<Report.ReportRule> getReportRule(String reportId) throws SQLException;
     @Select("select report_threshold_value from report_threshold_value where ruleid = #{ruleId} order by report_threshold_value asc")
     public List<Double> getReportThresholdValue(String ruleId) throws SQLException;
-    @Select("select reportid,reportname,orangealerts,redalerts,reportproducedate from report where templateid = #{templateId} orderby reportproducedate desc limit #{offect},#{limit}")
-    public List<ReportResult> getReports(String templateId,int offect,int limit) throws SQLException;
+    @Select("select reportid,reportname,orangealerts,redalerts,reportproducedate from report where templateid = #{templateId} order by reportproducedate desc limit #{limit} offset #{offset}")
+    public List<ReportResult> getReports(@Param("templateId") String templateId,@Param("offset") int offset,@Param("limit") int limit) throws SQLException;
     @Select("select systemrule.ruleid,rulename,ruleinfo,ruletype,rulecheckthresholdunit from systemrule,rule2datatype,rule2buildtype where systemrule.ruleid=rule2datatype.ruleid  and systemrule.ruleid=rule2buildtype.ruleid and buildtype=#{buildtype} and datatype=#{datatype} and ruletype=#{ruletype}")
     public List<TableColumnRules.SystemRule> getColumnSystemRules(int ruleType,int dataType,int buildType) throws SQLException;
     @Select("select systemrule.ruleid,rulename,ruleinfo,ruletype,rulecheckthresholdunit from systemrule,rule2buildtype where systemrule.ruleid=rule2buildtype.ruleid and buildtype=#{buildtype} and ruletype=#{ruletype}")
