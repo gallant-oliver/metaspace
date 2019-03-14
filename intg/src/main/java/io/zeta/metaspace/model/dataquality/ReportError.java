f82624073e125d1852562a9b8567b194511d38bc
sunhaoning <sunhaoning@gridsum.com>
2019-03-14 16:19:10 +0800

    修改【数据质量】规则运行重试机制并添加错误记录

---
+++ intg/src/main/java/io/zeta/metaspace/model/dataquality/ReportError.java
@@ -0,0 +1,89 @@
+// ======================================================================
+//
+//      Copyright (C) 北京国双科技有限公司
+//                    http://www.gridsum.com
+//
+//      保密性声明：此文件属北京国双科技有限公司所有，仅限拥有由国双科技
+//      授予了相应权限的人所查看和所修改。如果你没有被国双科技授予相应的
+//      权限而得到此文件，请删除此文件。未得国双科技同意，不得查看、修改、
+//      散播此文件。
+//
+//
+// ======================================================================
+/**
+ * @author sunhaoning@gridsum.com
+ * @date 2019/3/13 19:01
+ */
+package io.zeta.metaspace.model.dataquality;
+
+/*
+ * @description
+ * @author sunhaoning
+ * @date 2019/3/13 19:01
+ */
+public class ReportError {
+
+    private String errorId;
+    private String templateId;
+    private String reportId;
+    private String ruleId;
+    private String content;
+    private String generateTime;
+    private int retryCount;
+
+    public String getErrorId() {
+        return errorId;
+    }
+
+    public void setErrorId(String errorId) {
+        this.errorId = errorId;
+    }
+
+    public String getTemplateId() {
+        return templateId;
+    }
+
+    public void setTemplateId(String templateId) {
+        this.templateId = templateId;
+    }
+
+    public String getReportId() {
+        return reportId;
+    }
+
+    public void setReportId(String reportId) {
+        this.reportId = reportId;
+    }
+
+    public String getRuleId() {
+        return ruleId;
+    }
+
+    public void setRuleId(String ruleId) {
+        this.ruleId = ruleId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getGenerateTime() {
+        return generateTime;
+    }
+
+    public void setGenerateTime(String generateTime) {
+        this.generateTime = generateTime;
+    }
+
+    public int getRetryCount() {
+        return retryCount;
+    }
+
+    public void setRetryCount(int retryCount) {
+        this.retryCount = retryCount;
+    }
+}
