5b456f2de51a682477a0a54b61bc7a40b33d1565
sunhaoning <sunhaoning@gridsum.com>
2019-06-06 17:49:59 +0800

    更新前端

--- webapp/src/main/webapp/config.js
+++ webapp/src/main/webapp/config.js
@@ -9,22 +9,23 @@ window.systemConfigs = {
   'VERSION': 'v1.0.0',
 
   // API的地址
-  'API_HOST': 'http://localhost:21000/api/metaspace',
+  // 'API_HOST': 'http://10.201.50.209:80/api/metaspace',
 
   // SSO登录的地址
   'SSO_URL': 'http://10.200.60.36:8800',
 
   // SSOAPI地址
   'SSO_API': 'http://10.200.60.36:8800/api/v2',
-  'FIRST_LEVEL_ORGA':'00161824',
+
   // // SSOAPI地址
   // 'SSO_API': 'http://10.200.60.36:80/api/v2',
-  // 'API_HOST': 'http://192.168.10.57:21001/api/metaspace',
+  'API_HOST': 'http://10.201.50.209/api/metaspace',
   // 'SSO_API': 'http://192.168.10.58:10080/api/v2',
   // 'SSO_URL': 'http://192.168.10.58:10080',
   'Validate_KEY': 's-ticket',
   // SSO使用到关于ticket的http头部
   'TICKET_KEY': 'X-SSO-FullticketId',
+  'FIRST_LEVEL_ORGA': '00161824',
 
   // 用来显示页面的版权信息，一般显示在页面底部
   'LICENCE': '©Company 2018 版权所有'
