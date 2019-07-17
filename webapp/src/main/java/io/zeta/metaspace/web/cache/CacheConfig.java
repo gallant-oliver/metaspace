dc7272d4b95d47a1d5fb44d8f7c3e79059155453
sunhaoning <sunhaoning@gridsum.com>
2019-07-17 10:58:47 +0800

    redis缓存

---
+++ webapp/src/main/java/io/zeta/metaspace/web/cache/CacheConfig.java
@@ -0,0 +1,77 @@
+// ======================================================================
+//
+//      Copyright (C) 北京国双科技有限公司
+//                    http://www.gridsum.com
+//
+//      保密性声明：此文件属北京国双科技有限公司所有，仅限拥有由国双科技
+//      授予了相应权限的人所查看和所修改。如果你没有被国双科技授予相应的
+//      权限而得到此文件，请删除此文件。未得国双科技同意，不得查看、修改、
+//      散播此文件。
+//
+//
+// ======================================================================
+/**
+ * @author sunhaoning@gridsum.com
+ * @date 2019/7/16 18:11
+ */
+package io.zeta.metaspace.web.cache;
+
+import org.apache.atlas.ApplicationProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/*
+ * @description
+ * @author sunhaoning
+ * @date 2019/7/16 18:11
+ */
+@Configuration
+@EnableCaching
+public class CacheConfig extends CachingConfigurerSupport {
+
+    private static String hostName;
+    private static int port;
+
+    static {
+        try {
+        org.apache.commons.configuration.Configuration configuration = ApplicationProperties.get();
+        hostName = configuration.getString("metaspace.redis.host");
+        port = configuration.getInt("metaspace.redis.port");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+        JedisConnectionFactory redisConnectionFactory = new JedisConnectionFactory();
+
+        // Defaults
+        redisConnectionFactory.setHostName(hostName);
+        redisConnectionFactory.setPort(port);
+        return redisConnectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
+        redisTemplate.setConnectionFactory(cf);
+        return redisTemplate;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisTemplate redisTemplate) {
+        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+
+        // Number of seconds before expiration. Defaults to unlimited (0)
+        cacheManager.setDefaultExpiration(300);
+        return cacheManager;
+    }
+}
