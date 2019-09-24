37c217dc668705c21f6f51b7c2eb23572dea597b
朱学通 <zhuxuetong@gridsum.com>
2019-09-24 10:56:38 +0800

    Revert "Merge branch 'feature/1.5.0-bug-fix' into 'dev'"
    This reverts merge request !251

--- webapp/src/main/java/io/zeta/metaspace/web/util/FilterUtils.java
+++ webapp/src/main/java/io/zeta/metaspace/web/util/FilterUtils.java
@@ -13,8 +13,7 @@ public class FilterUtils {
                 || requestURL.contains("api/metaspace/role/roles/sso")
                 || requestURL.contains("api/metaspace/role/users/sso")
                 || requestURL.contains("api/metaspace/market/business")
-                || requestURL.contains("api/metaspace/metadata/refreshcache")
-                || requestURL.contains("api/metaspace/api")) {
+                || requestURL.contains("api/metaspace/metadata/refreshcache")) {
 
             return true;
         }
