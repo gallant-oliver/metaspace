282a620bbe245b0ed48d40229f2e63b30c9b2585
lixiang03 <lixiang03@gridsum.com>
2019-09-25 16:15:24 +0800

    修改查询表语句

--- repository/src/main/java/io/zeta/metaspace/utils/MetaspaceGremlin3QueryProvider.java
+++ repository/src/main/java/io/zeta/metaspace/utils/MetaspaceGremlin3QueryProvider.java
@@ -103,7 +103,7 @@ public class MetaspaceGremlin3QueryProvider extends MetaspaceGremlinQueryProvide
             case DB_TABLE_BY_STATE:
                 return "g.tx().commit();g.V().has('__typeName','hive_db').has('Asset.name', '%s').inE().outV().has('__typeName','hive_table').has('__guid').has('__state', '%s').order().by('__timestamp').dedup().toList()";
             case RDBMS_DB_TABLE_BY_STATE:
-                return "g.tx().commit();g.V().has('__typeName','rdbms_instance').has('guid', '%s').inE().outV().has('__typeName','rdbms_db').has('Asset.name', '%s').inE().outV().has('__typeName','rdbms_table').has('__guid').has('__state', '%s').order().by('__timestamp').dedup().toList()";
+                return "g.tx().commit();g.V().has('__typeName','rdbms_instance').has('__guid', '%s').inE().outV().has('__typeName','rdbms_db').has('Asset.name', '%s').inE().outV().has('__typeName','rdbms_table').has('__guid').has('__state', '%s').order().by('__timestamp').dedup().toList()";
             case FULL_DB_BY_STATE:
                 return "g.tx().commit();g.V().has('__typeName','hive_db').has('__guid').has('__state', '%s').order().by('__timestamp').dedup().toList()";
             case FULL_RDBMS_DB_BY_STATE:
