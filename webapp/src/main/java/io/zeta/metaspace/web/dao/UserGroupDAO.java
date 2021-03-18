239ccee5f431e64a89927bf8412d727915dc3525
wanghengzhang <wanghengzhang@gridsum.com>
2021-03-18 10:45:11 +0800

    菜单修改

--- webapp/src/main/java/io/zeta/metaspace/web/dao/UserGroupDAO.java
+++ webapp/src/main/java/io/zeta/metaspace/web/dao/UserGroupDAO.java
@@ -297,12 +297,13 @@ public interface UserGroupDAO {
             "</script>")
     public List<RoleModulesCategories.Category> getAllCategorysAndCount(@Param("categoryType") int categoryType,@Param("tenantId")String tenantId,@Param("dbNames") List<String> dbNames);
 
-    @Select("select t1.read,t1.edit_category,t1.edit_item,t2.guid, t2.name, t2.level, t2.qualifiedname, t2.parentcategoryguid, t2.upbrothercategoryguid, t2.downbrothercategoryguid,t2.description, t2.safe FROM category_group_relation t1 JOIN category t2 ON t1.category_id = t2.guid\n" +
+    @Select("select DISTINCT t1.read,t1.edit_category,t1.edit_item,t2.guid, t2.name, t2.level, t2.qualifiedname, t2.parentcategoryguid, t2.upbrothercategoryguid, t2.downbrothercategoryguid,t2.description, t2.safe FROM category_group_relation t1 JOIN category t2 ON t1.category_id = t2.guid\n" +
             "JOIN user_group t3 ON (t1.group_id = t3.id and t3.valid = true)\n" +
             "JOIN user_group_relation t4 ON t3.id = t4.group_id\n" +
             "WHERE t2.tenantid = #{tenantId}\n" +
             "AND t3.tenant = #{tenantId}\n" +
-            "AND t4.user_id = #{userId}")
+            "AND t4.user_id = #{userId}\n" +
+            "AND t2.categorytype = 0")
     public List<CategoryPrivilegeV2> getUserCategories(@Param("tenantId") String tenantId, @Param("userId") String userId);
 
     @Select("select c.guid from category_group_relation g join category c on g.category_id=c.guid where g.group_id=#{userGroupId} and c.categorytype=#{categoryType} and c.tenantid=#{tenantId} and g.read=true")
