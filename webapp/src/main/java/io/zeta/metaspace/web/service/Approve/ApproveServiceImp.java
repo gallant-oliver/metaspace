14bdc580c8e8f9ccf7323d5a9c6ab6555a0b331e
wangzisen <595581999@qq.com>
2021-03-29 20:26:14 +0800

    支持批量审批

--- webapp/src/main/java/io/zeta/metaspace/web/service/Approve/ApproveServiceImp.java
+++ webapp/src/main/java/io/zeta/metaspace/web/service/Approve/ApproveServiceImp.java
@@ -16,8 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import sun.jvm.hotspot.debugger.Page;
-
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
