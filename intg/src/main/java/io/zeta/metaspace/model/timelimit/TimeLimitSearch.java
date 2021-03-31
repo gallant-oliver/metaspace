4f19b8311ab053c5c6807689525665e7eca477ca
wangzisen <595581999@qq.com>
2021-03-31 11:32:01 +0800

    时间限定引用问题修复

--- intg/src/main/java/io/zeta/metaspace/model/timelimit/TimeLimitSearch.java
+++ intg/src/main/java/io/zeta/metaspace/model/timelimit/TimeLimitSearch.java
@@ -1,15 +1,12 @@
 package io.zeta.metaspace.model.timelimit;
 
-import io.zeta.metaspace.model.metadata.Parameters;
-import org.apache.commons.net.ntp.TimeStamp;
-
 import java.sql.Timestamp;
 import java.util.List;
 
 /**
  * 基于通过查询类派生出的时间限定查询类
  */
-public class TimeLimitSearch extends Parameters {
+public class TimeLimitSearch {
 
     public Timestamp getStartTime() {
         return startTime == null ? null :new Timestamp(Long.valueOf(startTime));
@@ -45,11 +42,61 @@ public class TimeLimitSearch extends Parameters {
      */
     private String id;
 
+    private String query;
+
+    private int offset;
+
+    private int limit;
+
+    private String sortBy;
+
+    private String order;
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
     public String getId() {
         return id;
     }
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getSortBy() {
+        return sortBy;
+    }
+
+    public void setSortBy(String sortBy) {
+        this.sortBy = sortBy;
     }
 }
