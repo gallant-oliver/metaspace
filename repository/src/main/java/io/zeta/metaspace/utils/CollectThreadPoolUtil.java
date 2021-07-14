db22a0a5551afdb42bf637eb918a7080f9710a5e
lvmengliang <lvmengliang@gridsum.com>
2021-07-14 21:24:00 +0800

    调整采集线程池参数

--- repository/src/main/java/io/zeta/metaspace/utils/CollectThreadPoolUtil.java
+++ repository/src/main/java/io/zeta/metaspace/utils/CollectThreadPoolUtil.java
@@ -18,13 +18,13 @@ public class CollectThreadPoolUtil {
     public static final int CORE_POOL_SIZE = 10;
 
     /** 线程池最大线程数 */
-    public static final int MAX_POOL_SIZE = 20;
+    public static final int MAX_POOL_SIZE = 25;
 
     /** 空闲线程回收时间 */
     public static final int KEEP_ALIVE_TIME = 1000;
 
     /** 线程池等待队列 */
-    public static final int BLOCKING_QUEUE_SIZE = 1000;
+    public static final int BLOCKING_QUEUE_SIZE = 1500;
 
     /** 业务请求异步处理线程池 */
     private static final ThreadPoolExecutor processExecutor = new ThreadPoolExecutor(
