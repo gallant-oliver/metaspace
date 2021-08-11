41da55562bfefd687a72391bd50e022c92888580
guowenkui <guowenkui@gridsum.com>
2021-08-11 16:49:46 +0800

    sql语句查询问题修复

--- webapp/src/main/java/io/zeta/metaspace/web/dao/sourceinfo/DatabaseDAO.java
+++ webapp/src/main/java/io/zeta/metaspace/web/dao/sourceinfo/DatabaseDAO.java
@@ -73,7 +73,7 @@ public interface DatabaseDAO {
     @Select("<script>"+
             "select tb.db_type AS dbType,tb.database_guid AS databaseId,tb.database_name AS databaseName ," +
             "  (select source_id from source_db where db_guid=tb.database_guid limit 1 ) AS sourceId "+
-            "from db_info tb where database_name in "+
+            "from db_info tb where status='ACTIVE' and database_name in "+
             "<foreach collection='dbNameList' item='dbName' separator=',' open='(' close=')'>"+
             "#{dbName}"+
             "</foreach>"+
