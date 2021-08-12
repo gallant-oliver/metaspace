15db68937ae27bf000c14f9bd69237ef381d5ccb
guowenkui <guowenkui@gridsum.com>
2021-08-12 14:07:09 +0800

    处理数据库模板缺失数据源等字段问题

--- webapp/src/main/java/io/zeta/metaspace/web/util/PoiExcelUtils.java
+++ webapp/src/main/java/io/zeta/metaspace/web/util/PoiExcelUtils.java
@@ -554,14 +554,21 @@ public class PoiExcelUtils {
      * @param data 值包含分号; 的则使用下拉框处理
      * @return
      */
-    public static Workbook createExcelFileWithDropDown(List<String> attributes, List<String> data,String sheetName) {
+    public static Workbook createExcelFileWithDropDown(List<String> attributes, List<Object> data,String sheetName) {
         // 1. 创建workbook
         XSSFWorkbook workbook = new XSSFWorkbook();
         XSSFSheet sheet = workbook.createSheet(CustomStringUtils.handleExcelName(sheetName));
+        int pIndex = 0, curIndex = 0;
         //标题头
         Row row0 = sheet.createRow(0);
         for (int i = 0; i < attributes.size(); i++) {
             String title = attributes.get(i).trim();
+            if("数据库类型".equalsIgnoreCase(title)){
+                pIndex = i;
+            }
+            if("数据源".equalsIgnoreCase(title)){
+                curIndex = i;
+            }
             Cell cell = row0.createCell(i);
             sheet.setColumnWidth(i,title.length()*520);
             cell.setCellValue(title);
@@ -576,17 +583,26 @@ public class PoiExcelUtils {
             Row row = sheet.createRow(1);
             int colSize = data.size();
             for (int i = 0; i < colSize; i++) {
-                String colInfo = data.get(i);
-                if(colInfo.contains(";")){
-                    String[] datas = colInfo.split(";");
-                     XSSFDataValidationConstraint dvConstraint = (XSSFDataValidationConstraint) dvHelper
+                Object obj = data.get(i);
+                if(obj instanceof String){
+                    row.createCell(i).setCellValue(obj.toString());
+                }
+                if(obj instanceof List){
+                    List<String> list = (List<String>) obj;
+                    String[] datas = list.toArray(new String[list.size()]);
+                    XSSFDataValidationConstraint dvConstraint = (XSSFDataValidationConstraint) dvHelper
                             .createExplicitListConstraint(datas);
                     addressList = new CellRangeAddressList(1, 100, i, i);
                     validation = (XSSFDataValidation) dvHelper.createValidation(
                             dvConstraint, addressList);
                     sheet.addValidationData(validation);
-                }else{
-                    row.createCell(i).setCellValue(colInfo);
+                }
+                if(obj instanceof Map){
+                    Map<String, Set<String>> dataSourceMap = (Map<String, Set<String>>) obj;
+                    if(dataSourceMap != null && !dataSourceMap.isEmpty()){
+                        Set<String> set = dataSourceMap.keySet();
+                        addCascade(workbook,sheet,set.toArray(new String[set.size()]),dataSourceMap,pIndex,curIndex);
+                    }
                 }
 
             }
@@ -594,4 +610,149 @@ public class PoiExcelUtils {
 
         return workbook;
     }
+
+    private static void addCascade(XSSFWorkbook book,XSSFSheet sheetPro,String[] fathers,Map<String,Set<String>> areaMap,int pIndex,int curIndex){
+        Sheet hideSheet = book.createSheet("area");
+        //这一行作用是将此sheet隐藏，功能未完成时注释此行,可以查看隐藏sheet中信息是否正确
+        book.setSheetHidden(book.getSheetIndex(hideSheet), true);
+
+        int rowId = 0;
+        // 设置第一行，存省的信息
+        Row provinceRow = hideSheet.createRow(rowId++);
+        provinceRow.createCell(0).setCellValue("数据源类型");
+        for(int i = 0; i < fathers.length; i ++){
+            Cell provinceCell = provinceRow.createCell(i + 1);
+            provinceCell.setCellValue(fathers[i]);
+        }
+        // 将具体的数据写入到每一行中，行开头为父级区域，后面是子区域。
+        for(int i = 0;i < fathers.length;i++){
+            String key = fathers[i];
+            Set<String> set = areaMap.get(key);
+            String[] son = set.toArray(new String[set.size()]);
+            Row row = hideSheet.createRow(rowId++);
+            row.createCell(0).setCellValue(key);
+            for(int j = 0; j < son.length; j ++){
+                Cell cell = row.createCell(j + 1);
+                cell.setCellValue(son[j]);
+            }
+
+            // 添加名称管理器
+            String range = getRange(1, rowId, son.length);
+            Name name = book.createName();
+            //key不可重复
+            name.setNameName(key);
+            String formula = "area!" + range;
+            name.setRefersToFormula(formula);
+        }
+        XSSFDataValidationHelper dvHelper = new XSSFDataValidationHelper((XSSFSheet)sheetPro);
+
+        DataValidationConstraint provConstraint = dvHelper.createExplicitListConstraint(fathers);
+        // 四个参数分别是：起始行、终止行、起始列、终止列
+        CellRangeAddressList provRangeAddressList = new CellRangeAddressList(1, 100, pIndex, pIndex);
+        DataValidation provinceDataValidation = dvHelper.createValidation(provConstraint, provRangeAddressList);
+        //验证
+        provinceDataValidation.createErrorBox("error", "请选择正确的类型");
+        provinceDataValidation.setShowErrorBox(true);
+        provinceDataValidation.setSuppressDropDownArrow(true);
+        sheetPro.addValidationData(provinceDataValidation);
+
+        //对前100行设置有效性
+        for(int i = 2;i < 100;i++){
+            setDataValidation(getAlphabet(pIndex) ,sheetPro,i,curIndex+1);
+           // setDataValidation("B" ,sheetPro,i,3);
+        }
+
+    }
+    private static String getAlphabet(int index){
+        String[] strArr = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+        return strArr[index];
+    }
+    /**
+     * 设置有效性
+     * @param offset 主影响单元格所在列，即此单元格由哪个单元格影响联动
+     * @param sheet
+     * @param rowNum 行数
+     * @param colNum 列数
+     */
+    public static void setDataValidation(String offset,XSSFSheet sheet, int rowNum,int colNum) {
+        XSSFDataValidationHelper dvHelper = new XSSFDataValidationHelper(sheet);
+        DataValidation data_validation_list;
+        data_validation_list = getDataValidationByFormula(
+                "INDIRECT($" + offset + (rowNum) + ")", rowNum, colNum,dvHelper);
+        sheet.addValidationData(data_validation_list);
+    }
+    /**
+     * 加载下拉列表内容
+     * @param formulaString
+     * @param naturalRowIndex
+     * @param naturalColumnIndex
+     * @param dvHelper
+     * @return
+     */
+    private static  DataValidation getDataValidationByFormula(
+            String formulaString, int naturalRowIndex, int naturalColumnIndex,XSSFDataValidationHelper dvHelper) {
+        // 加载下拉列表内容
+        // 举例：若formulaString = "INDIRECT($A$2)" 表示规则数据会从名称管理器中获取key与单元格 A2 值相同的数据，
+        //如果A2是江苏省，那么此处就是江苏省下的市信息。
+        XSSFDataValidationConstraint dvConstraint = (XSSFDataValidationConstraint) dvHelper.createFormulaListConstraint(formulaString);
+        // 设置数据有效性加载在哪个单元格上。
+        // 四个参数分别是：起始行、终止行、起始列、终止列
+        int firstRow = naturalRowIndex -1;
+        int lastRow = naturalRowIndex - 1;
+        int firstCol = naturalColumnIndex - 1;
+        int lastCol = naturalColumnIndex - 1;
+        CellRangeAddressList regions = new CellRangeAddressList(firstRow,
+                lastRow, firstCol, lastCol);
+        // 数据有效性对象
+        // 绑定
+        XSSFDataValidation data_validation_list = (XSSFDataValidation) dvHelper.createValidation(dvConstraint, regions);
+        data_validation_list.setEmptyCellAllowed(false);
+        if (data_validation_list instanceof XSSFDataValidation) {
+            data_validation_list.setSuppressDropDownArrow(true);
+            data_validation_list.setShowErrorBox(true);
+        } else {
+            data_validation_list.setSuppressDropDownArrow(false);
+        }
+        // 设置输入信息提示信息
+        data_validation_list.createPromptBox("下拉选择提示", "请使用下拉方式选择合适的值！");
+        // 设置输入错误提示信息
+        //data_validation_list.createErrorBox("选择错误提示", "你输入的值未在备选列表中，请下拉选择合适的值！");
+        return data_validation_list;
+    }
+
+    /**
+     *  计算formula
+     * @param offset 偏移量，如果给0，表示从A列开始，1，就是从B列
+     * @param rowId 第几行
+     * @param colCount 一共多少列
+     * @return 如果给入参 1,1,10. 表示从B1-K1。最终返回 $B$1:$K$1
+     *
+     */
+    public static String getRange(int offset, int rowId, int colCount) {
+        char start = (char)('A' + offset);
+        if (colCount <= 25) {
+            char end = (char)(start + colCount - 1);
+            return "$" + start + "$" + rowId + ":$" + end + "$" + rowId;
+        } else {
+            char endPrefix = 'A';
+            char endSuffix = 'A';
+            if ((colCount - 25) / 26 == 0 || colCount == 51) {// 26-51之间，包括边界（仅两次字母表计算）
+                if ((colCount - 25) % 26 == 0) {// 边界值
+                    endSuffix = (char)('A' + 25);
+                } else {
+                    endSuffix = (char)('A' + (colCount - 25) % 26 - 1);
+                }
+            } else {// 51以上
+                if ((colCount - 25) % 26 == 0) {
+                    endSuffix = (char)('A' + 25);
+                    endPrefix = (char)(endPrefix + (colCount - 25) / 26 - 1);
+                } else {
+                    endSuffix = (char)('A' + (colCount - 25) % 26 - 1);
+                    endPrefix = (char)(endPrefix + (colCount - 25) / 26);
+                }
+            }
+            return "$" + start + "$" + rowId + ":$" + endPrefix + endSuffix + "$" + rowId;
+        }
+    }
+
 }
