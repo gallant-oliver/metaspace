2a79f78aae3cccff308f36133e97f866faf36af2
haobiao <haobiao@gridsum.com>
2021-10-18 19:38:05 +0800

    增加接口

--- webapp/src/main/java/io/zeta/metaspace/web/rest/PublicREST.java
+++ webapp/src/main/java/io/zeta/metaspace/web/rest/PublicREST.java
@@ -1,13 +1,18 @@
 package io.zeta.metaspace.web.rest;
 
 import io.zeta.metaspace.model.Result;
+import io.zeta.metaspace.model.business.BusinessInfo;
 import io.zeta.metaspace.model.business.BusinessInfoHeader;
 import io.zeta.metaspace.model.metadata.Parameters;
 import io.zeta.metaspace.model.metadata.RelationQuery;
 import io.zeta.metaspace.model.result.PageResult;
+import io.zeta.metaspace.model.share.APIInfoHeader;
+import io.zeta.metaspace.model.share.ApiHead;
+import io.zeta.metaspace.web.service.BusinessService;
 import io.zeta.metaspace.web.service.PublicService;
 import io.zeta.metaspace.web.service.sourceinfo.SourceInfoDatabaseService;
 import io.zeta.metaspace.web.util.ReturnUtil;
+import org.apache.atlas.AtlasErrorCode;
 import org.apache.atlas.exception.AtlasBaseException;
 import org.apache.atlas.model.metadata.RelationEntityV2;
 import org.apache.atlas.web.util.Servlets;
@@ -31,6 +36,9 @@ public class PublicREST {
 
     @Autowired
     private SourceInfoDatabaseService sourceInfoDatabaseService;
+
+    @Autowired
+    private BusinessService businessService;
 
     /**
      * 获取所有目录
@@ -119,5 +127,97 @@ public class PublicREST {
     @Consumes(Servlets.JSON_MEDIA_TYPE)
     public Result getSourceInfoDetail(@PathParam("tenantId") String tenantId, @PathParam("id") String id, @QueryParam("version") @DefaultValue("0") String version) {
         return sourceInfoDatabaseService.getDatabaseInfoById(id, tenantId, Integer.parseInt(version));
+    }
+
+    /**
+     * 业务对象详情
+     *
+     * @param businessId
+     * @return
+     * @throws AtlasBaseException
+     */
+    @GET
+    @Path("/{businessId}/{tenantId}")
+    @Consumes(Servlets.JSON_MEDIA_TYPE)
+    @Produces(Servlets.JSON_MEDIA_TYPE)
+    public BusinessInfo getBusiness(@PathParam("businessId") String businessId, @PathParam("tenantId") String tenantId) throws AtlasBaseException {
+        try {
+            return businessService.getBusinessInfo(businessId, tenantId);
+        } catch (Exception e) {
+            throw new AtlasBaseException(e.getMessage(), AtlasErrorCode.BAD_REQUEST, e, "获取业务对象列表失败");
+        }
+    }
+
+    /**
+     * 获取表详情
+     *
+     * @param guid
+     * @return
+     * @throws AtlasBaseException
+     */
+    @GET
+    @Path("/table/{guid}/{tenantId}")
+    @Consumes(Servlets.JSON_MEDIA_TYPE)
+    @Produces(Servlets.JSON_MEDIA_TYPE)
+    public Object getTableInfoById(@PathParam("guid") String guid, @PathParam("tenantId") String tenantId) throws AtlasBaseException {
+        try {
+            return businessService.getTableInfoById(guid, tenantId);
+        } catch (Exception e) {
+            throw new AtlasBaseException(e.getMessage(), AtlasErrorCode.BAD_REQUEST, e, "获取表详情失败");
+        }
+    }
+
+    /**
+     * 获取关联的API列表
+     *
+     * @param businessId
+     * @param parameters
+     * @param tenantId
+     * @return
+     * @throws AtlasBaseException
+     * @throws AtlasBaseException
+     */
+    @POST
+    @Path("/{businessId}/datashare/{tenantId}")
+    @Consumes(Servlets.JSON_MEDIA_TYPE)
+    @Produces(Servlets.JSON_MEDIA_TYPE)
+    public PageResult<APIInfoHeader> getBusinessTableRelatedAPI(@PathParam("businessId") String businessId, Parameters parameters, @PathParam("tenantId") String tenantId) throws AtlasBaseException {
+        try {
+            return businessService.getBusinessTableRelatedAPI(businessId, parameters, tenantId);
+        } catch (Exception e) {
+            throw new AtlasBaseException(e.getMessage(), AtlasErrorCode.BAD_REQUEST, e, "获取关联API失败");
+        }
+    }
+
+    /**
+     * 业务对象api展示列表
+     *
+     * @param businessId
+     * @param isNew
+     * @param up
+     * @param down
+     * @param limit
+     * @param offset
+     * @param tenantId
+     * @return
+     * @throws AtlasBaseException
+     */
+    @GET
+    @Path("/{businessId}/dataservice/{tenantId}")
+    @Consumes(Servlets.JSON_MEDIA_TYPE)
+    @Produces(Servlets.JSON_MEDIA_TYPE)
+    public Result getBusinessTableRelatedDataServiceAPI(@PathParam("businessId") String businessId, @DefaultValue("false") @QueryParam("new") boolean isNew,
+                                                        @DefaultValue("true") @QueryParam("up") boolean up, @DefaultValue("true") @QueryParam("down") boolean down,
+                                                        @DefaultValue("-1") @QueryParam("limit") int limit, @DefaultValue("0") @QueryParam("offset") int offset,
+                                                        @PathParam("tenantId") String tenantId) throws AtlasBaseException {
+        try {
+            Parameters parameters = new Parameters();
+            parameters.setLimit(limit);
+            parameters.setOffset(offset);
+            PageResult<ApiHead> pageResult = businessService.getBusinessTableRelatedDataServiceAPI(businessId, parameters, isNew, up, down, tenantId);
+            return ReturnUtil.success(pageResult);
+        } catch (Exception e) {
+            throw new AtlasBaseException(e.getMessage(), AtlasErrorCode.BAD_REQUEST, e, "获取api展示列表失败");
+        }
     }
 }
