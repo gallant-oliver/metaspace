53acd07b87c135c45c1e1d6794e2584b9cc8a23c
wuqianhe <Wqh3399308>
2021-10-20 16:11:06 +0800

    FIX:修复用户组判断逻辑问题

--- webapp/src/main/java/io/zeta/metaspace/web/dao/GroupDeriveTableRelationDAO.java
+++ webapp/src/main/java/io/zeta/metaspace/web/dao/GroupDeriveTableRelationDAO.java
@@ -103,24 +103,24 @@ public interface GroupDeriveTableRelationDAO {
             " LEFT JOIN businessinfo bi ON bi.businessid = bt.businessid " +
             " LEFT JOIN business_relation br ON br.businessid = bi.businessid " +
             " LEFT JOIN category c ON c.guid = br.categoryguid " +
-            " LEFT JOIN group_table_relation gtr ON gtr.derive_table_id = ti.tableguid AND  gtr.user_group_id = #{userGroupId}  " +
-            "<if test='privilegeType == \"IMPORTANCE\" and registerType == false'>" +
-            "   AND ( gtr.importance_privilege != true OR gtr.importance_privilege IS NULL) " +
-            "</if>"+
-            "<if test='privilegeType == \"IMPORTANCE\" and registerType == true'>" +
-            "   AND gtr.importance_privilege = true  " +
-            "</if>"+
-            "<if test='privilegeType == \"SECURITY\" and registerType == false'>" +
-            "   AND (gtr.security_privilege != true OR gtr.security_privilege IS NULL) " +
-            "</if>"+
-            "<if test='privilegeType == \"SECURITY\" and registerType  == true'>" +
-            "   AND gtr.security_privilege = true  " +
-            "</if>"+
-            "<if test='privilegeType == \"ALL\" and registerType == true'>" +
-            "   AND (gtr.security_privilege = true OR gtr.importance_privilege = true) " +
-            "</if>"+
+            " LEFT JOIN group_table_relation gtr ON gtr.derive_table_id = ti.tableguid " +
             "WHERE " +
             " sidti.tenant_id = #{tenantId}  " +
+            "<if test='privilegeType == \"IMPORTANCE\" and registerType == false'>" +
+            "   AND ( (gtr.importance_privilege != true  AND  gtr.user_group_id = #{userGroupId} ) OR gtr.importance_privilege IS NULL)  " +
+            "</if>"+
+            "<if test='privilegeType == \"IMPORTANCE\" and registerType == true'>" +
+            "   AND gtr.importance_privilege = true  AND  gtr.user_group_id = #{userGroupId}  " +
+            "</if>"+
+            "<if test='privilegeType == \"SECURITY\" and registerType == false'>" +
+            "   AND ((gtr.security_privilege != true  AND  gtr.user_group_id = #{userGroupId}  )OR gtr.security_privilege IS NULL) " +
+            "</if>"+
+            "<if test='privilegeType == \"SECURITY\" and registerType  == true'>" +
+            "   AND gtr.security_privilege = true  AND  gtr.user_group_id = #{userGroupId}   " +
+            "</if>"+
+            "<if test='privilegeType == \"ALL\" and registerType == true'>" +
+            "   AND (gtr.security_privilege = true OR gtr.importance_privilege = true)  AND  gtr.user_group_id = #{userGroupId}  " +
+            "</if>"+
             "<if test='tableName != null and tableName !=\"\"'>" +
             " AND sidti.table_name_zh like '%'||#{tableName}||'%' ESCAPE '/' " +
             "</if>" +
