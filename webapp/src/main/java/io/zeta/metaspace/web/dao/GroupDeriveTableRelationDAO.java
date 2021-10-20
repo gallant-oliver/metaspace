7e86a7fa03e95f0295a55e0f7da97a3ca444703e
wuqianhe <Wqh3399308>
2021-10-20 14:18:47 +0800

    FIX:修复衍生表版本问题，补充version = -1

--- webapp/src/main/java/io/zeta/metaspace/web/dao/GroupDeriveTableRelationDAO.java
+++ webapp/src/main/java/io/zeta/metaspace/web/dao/GroupDeriveTableRelationDAO.java
@@ -37,47 +37,47 @@ public interface GroupDeriveTableRelationDAO {
                                                    @Param("userGroupIds") List<String> userGroups,
                                                    @Param("tenantId")String tenantId);
 
-    @Update("UPDATE group_table_relation \n" +
-            "SET importance_privilege = NULL \n" +
-            "WHERE\n" +
+    @Update("UPDATE group_table_relation " +
+            "SET importance_privilege = NULL " +
+            "WHERE" +
             " user_group_id = #{userGroupId} AND tenant_id = #{tenantId}")
     void updateImportancePrivilegeNullByUserGroupId(@Param("userGroupId") String userGroupId, @Param("tenantId")String tenantId);
 
-    @Update("UPDATE group_table_relation \n" +
-            "SET security_privilege = NULL \n" +
-            "WHERE\n" +
+    @Update("UPDATE group_table_relation " +
+            "SET security_privilege = NULL " +
+            "WHERE" +
             " user_group_id = #{userGroupId} AND tenant_id = #{tenantId}")
     void updateSecurityPrivilegeNullByUserGroupId(@Param("userGroupId") String userGroupId, @Param("tenantId")String tenantId);
 
     @Update("<script>" +
-            "INSERT INTO group_table_relation ( ID, derive_table_id, importance_privilege, user_group_id, tenant_id )\n" +
-            "VALUES\n" +
+            "INSERT INTO group_table_relation ( ID, derive_table_id, importance_privilege, user_group_id, tenant_id )" +
+            "VALUES" +
             " <foreach item='relation' index='index' collection='relations' separator=',' open='(' close=')'>" +
             " ( #{ relation.id }, #{ relation.deriveTableId }, TRUE, #{ relation.userGroupId }, #{ relation.tenantId } ) ON conflict ( user_group_id, derive_table_id, tenant_id ) " +
             "</foreach>" +
-            "DO\n" +
-            "UPDATE \n" +
+            "DO" +
+            "UPDATE " +
             " SET importance_privilege = excluded.importance_privilege" +
             "</script>")
     void updateDeriveTableImportancePrivilege(@Param("relations") List<GroupDeriveTableRelation> relationList);
 
     @Update("<script>" +
-            "INSERT INTO group_table_relation ( ID, derive_table_id, security_privilege, user_group_id, tenant_id )\n" +
-            "VALUES\n" +
+            "INSERT INTO group_table_relation ( ID, derive_table_id, security_privilege, user_group_id, tenant_id )" +
+            "VALUES" +
             " <foreach item='relation' index='index' collection='relations' separator=',' open='(' close=')'>" +
             " ( #{ relation.id }, #{ relation.deriveTableId }, TRUE, #{ relation.userGroupId }, #{ relation.tenantId } ) ON conflict ( user_group_id, derive_table_id, tenant_id ) " +
             "</foreach>" +
-            "DO\n" +
-            "UPDATE \n" +
+            "DO" +
+            "UPDATE " +
             " SET security_privilege = excluded.security_privilege" +
             "</script>")
     void updateDeriveTableSecurityPrivilege(@Param("relations") List<GroupDeriveTableRelation> relationList);
     
     @Delete("<script>" +
-            "DELETE \n" +
-            "FROM\n" +
-            " group_table_relation \n" +
-            "WHERE\n" +
+            "DELETE " +
+            "FROM" +
+            " group_table_relation " +
+            "WHERE" +
             " id IN " +
             " <foreach item='id' index='ids' collection='relations' separator=',' open='(' close=')'>" +
             "#{id}" +
@@ -87,45 +87,45 @@ public interface GroupDeriveTableRelationDAO {
 
     @Select("<script>" +
             "SELECT " +
-            "count(*) over() AS total,\n" +
+            "count(*) over() AS total," +
             " ti.tableguid AS tableId," +
-            " ti.tablename AS tableNameEn,\n" +
-            "  sidti.table_name_zh AS tableNameZn,\n" +
-            " gtr.importance_privilege AS importancePrivilege,\n" +
-            " gtr.security_privilege AS securityPrivilege,\n" +
-            " gtr.\"id\" AS groupTableRelationId,\n" +
-            " bi.name AS businessObjectName,\n" +
-            " c.name AS businessCategoryName\n" +
-            "FROM\n" +
-            " tableinfo ti \n" +
-            " INNER JOIN source_info_derive_table_info sidti ON sidti.table_name_en = ti.tablename AND sidti.db_id = ti.databaseguid \n" +
-            " LEFT JOIN business2table bt ON bt.tableguid = ti.tableguid\n" +
-            " LEFT JOIN businessinfo bi ON bi.businessid = bt.businessid\n" +
-            " LEFT JOIN business_relation br ON br.businessid = bi.businessid\n" +
-            " LEFT JOIN category c ON c.guid = br.categoryguid\n" +
-            " LEFT JOIN group_table_relation gtr ON gtr.derive_table_id = ti.tableguid AND  gtr.user_group_id = #{userGroupId} \n" +
+            " ti.tablename AS tableNameEn," +
+            "  sidti.table_name_zh AS tableNameZn," +
+            " gtr.importance_privilege AS importancePrivilege," +
+            " gtr.security_privilege AS securityPrivilege," +
+            " gtr.\"id\" AS groupTableRelationId," +
+            " bi.name AS businessObjectName," +
+            " c.name AS businessCategoryName" +
+            "FROM" +
+            " tableinfo ti " +
+            " INNER JOIN source_info_derive_table_info sidti ON sidti.table_name_en = ti.tablename AND sidti.db_id = ti.databaseguid AND sidti.version = -1" +
+            " LEFT JOIN business2table bt ON bt.tableguid = ti.tableguid" +
+            " LEFT JOIN businessinfo bi ON bi.businessid = bt.businessid" +
+            " LEFT JOIN business_relation br ON br.businessid = bi.businessid" +
+            " LEFT JOIN category c ON c.guid = br.categoryguid" +
+            " LEFT JOIN group_table_relation gtr ON gtr.derive_table_id = ti.tableguid AND  gtr.user_group_id = #{userGroupId} " +
             "<if test='privilegeType == \"IMPORTANCE\" and registerType == false'>" +
-            "   AND ( gtr.importance_privilege != true OR gtr.importance_privilege IS NULL)\n" +
+            "   AND ( gtr.importance_privilege != true OR gtr.importance_privilege IS NULL)" +
             "</if>"+
             "<if test='privilegeType == \"IMPORTANCE\" and registerType == true'>" +
-            "   AND gtr.importance_privilege = true \n" +
+            "   AND gtr.importance_privilege = true " +
             "</if>"+
             "<if test='privilegeType == \"SECURITY\" and registerType == false'>" +
-            "   AND (gtr.security_privilege != true OR gtr.security_privilege IS NULL)\n" +
+            "   AND (gtr.security_privilege != true OR gtr.security_privilege IS NULL)" +
             "</if>"+
             "<if test='privilegeType == \"SECURITY\" and registerType  == true'>" +
-            "   AND gtr.security_privilege = true \n" +
+            "   AND gtr.security_privilege = true " +
             "</if>"+
             "<if test='privilegeType == \"ALL\" and registerType == true'>" +
-            "   AND (gtr.security_privilege = true OR gtr.importance_privilege = true)\n" +
+            "   AND (gtr.security_privilege = true OR gtr.importance_privilege = true)" +
             "</if>"+
-            "WHERE\n" +
-            " sidti.tenant_id = #{tenantId} \n" +
+            "WHERE" +
+            " sidti.tenant_id = #{tenantId} " +
             "<if test='tableName != null and tableName !=\"\"'>" +
-            " AND sidti.table_name_zh like '%'||#{tableName}||'%' ESCAPE '/'\n" +
+            " AND sidti.table_name_zh like '%'||#{tableName}||'%' ESCAPE '/'" +
             "</if>" +
             "<if test = 'limit &gt; 0'>"+
-            " LIMIT #{limit} OFFSET #{offset}\n" +
+            " LIMIT #{limit} OFFSET #{offset}" +
             "</if>"+
             "</script>")
     List<GroupDeriveTableInfo> getRelationInfos(@Param("tenantId") String tenantId, @Param("privilegeType") String privilegeType,
