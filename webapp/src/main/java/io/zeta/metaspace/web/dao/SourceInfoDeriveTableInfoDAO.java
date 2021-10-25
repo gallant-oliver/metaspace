405a160131f72b88a540e98ccb856a82dc7c9c0b
wuyongliang <wuyongliang@gridsum.com>
2021-10-25 18:55:42 +0800

    衍生表登记问题

--- webapp/src/main/java/io/zeta/metaspace/web/dao/SourceInfoDeriveTableInfoDAO.java
+++ webapp/src/main/java/io/zeta/metaspace/web/dao/SourceInfoDeriveTableInfoDAO.java
@@ -231,7 +231,7 @@ public interface SourceInfoDeriveTableInfoDAO {
     List<TableExtInfo> getImportanceInfo( @Param("tableGuid") String tableGuid,@Param("tenantId") String tenantId);
 
     @Select("<script>" +
-            "select business_id businessId, source_table_guid sourceTableGuid, source_id sourceId " +
+            "select business_id businessId, table_guid tableGuid, source_id sourceId " +
             "from source_info_derive_table_info " +
             "where state=1 and table_guid in " +
             " <foreach item='tableGuid' index='index' collection='tableGuids' separator=',' open='(' close=')'>" +
@@ -240,15 +240,17 @@ public interface SourceInfoDeriveTableInfoDAO {
             "</script>")
     List<SourceInfoDeriveTableInfo> getDeriveTableInfoByGuids(@Param("tableGuids")List<String> tableGuids);
 
-    @Select("SELECT * FROM source_info_derive_table_info WHERE db_id = #{dbId} AND table_name_en = #{tableName}")
-    List<SourceInfoDeriveTableInfo> selectByDbAndTableName(@Param("dbId") String dbId, @Param("tableName") String tableName);
+    @Select("SELECT * FROM source_info_derive_table_info WHERE db_id = #{dbId} AND table_name_en = #{tableName} and version=-1")
+    SourceInfoDeriveTableInfo selectByDbAndTableName(@Param("dbId") String dbId, @Param("tableName") String tableName);
 
     @Update("update source_info_derive_table_info SET table_guid = #{tableGuid},update_time = now() WHERE db_id = #{dbId} AND table_name_en = #{tableName}")
     int updateByDbAndTableName(@Param("dbId") String dbId, @Param("tableName") String tableName, @Param("tableGuid") String tableGuid);
 
-    @Select("SELECT * FROM source_info_derive_table_info WHERE table_guid = #{tableGuid}")
-    List<SourceInfoDeriveTableInfo> selectByTableGuid(@Param("tableGuid") String table_guid);
+    @Select("SELECT * FROM source_info_derive_table_info WHERE table_guid = #{tableGuid} and version=-1")
+    SourceInfoDeriveTableInfo selectByTableGuid(@Param("tableGuid") String table_guid);
 
-    @Update("update source_info_derive_table_info set table_name_en = #{tableName},update_time = now() WHERE table_guid = #{tableGuid}")
-    int updateByTableGuid(@Param("tableGuid") String table_guid, @Param("tableName") String tableName);
+    @Update("update source_info_derive_table_info set table_name_en = #{tableName},update_time = now(), " +
+            "ddl=#{ddl}, dml=#{dml} " +
+            "WHERE table_guid = #{tableGuid}")
+    int updateByTableGuid(@Param("tableGuid") String table_guid, @Param("tableName") String tableName, @Param("ddl") String ddl, @Param("dml") String dml);
 }
