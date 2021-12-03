72d827e8a86ae023b0ec82cdb3f603a4116263dd
wuqianhe <Wqh3399308>
2021-12-03 17:51:40 +0800

    FIX：优化来源系统查询

--- intg/src/main/java/io/zeta/metaspace/model/enums/BusinessType.java
+++ intg/src/main/java/io/zeta/metaspace/model/enums/BusinessType.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -19,15 +18,15 @@ public enum BusinessType {
      * 新建
      */
 
-    ATOMIC_INDEX("1","原子指标"),
-    DERIVE_INDEX("2","衍生指标"),
-    COMPOSITE_INDEX("3","复合指标"),
-    TIME_LIMIT("8","时间限定"),
-    BUSINESS_INDEX("9","业务指标"),
-    DATABASE_INFO_REGISTER("4","数据库登记"),
-    BUSINESSCATALOGUE("5","业务目录"),
-    BUSINESS_OBJECT("6","业务对象"),
-    INDEXCATALOGUE("7","指标目录");
+    ATOMIC_INDEX("1","原子指标", SourceSystem.INDICATORS_LIB),
+    DERIVE_INDEX("2","衍生指标", SourceSystem.INDICATORS_LIB),
+    COMPOSITE_INDEX("3","复合指标", SourceSystem.INDICATORS_LIB),
+    TIME_LIMIT("8","时间限定", SourceSystem.INDICATORS_LIB),
+    BUSINESS_INDEX("9","业务指标", SourceSystem.INDICATORS_LIB),
+    DATABASE_INFO_REGISTER("4","数据库登记", SourceSystem.METASPACE),
+    BUSINESSCATALOGUE("5","业务目录", SourceSystem.METASPACE),
+    BUSINESS_OBJECT("6","业务对象", SourceSystem.METASPACE),
+    INDEXCATALOGUE("7","指标目录", SourceSystem.INDICATORS_LIB);
 
 
 
@@ -35,9 +34,12 @@ public enum BusinessType {
 
     private final String typeText;
 
-    BusinessType(String typeCode, String typeText) {
+    private final SourceSystem sourceSystem;
+
+    BusinessType(String typeCode, String typeText, SourceSystem sourceSystem) {
         this.typeCode = typeCode;
         this.typeText = typeText;
+        this.sourceSystem = sourceSystem;
     }
 
     public static String getTextByCode(String code){
@@ -46,5 +48,12 @@ public enum BusinessType {
             return null;
         }
         return businessTypes.get(0).getTypeText();
+    }
+    public static SourceSystem getSystem(String code){
+        List<BusinessType> businessTypes=Arrays.stream(BusinessType.values()).filter(b->b.getTypeCode().equals(code)).collect(Collectors.toList());
+        if (businessTypes.isEmpty()){
+            return null;
+        }
+        return businessTypes.get(0).getSourceSystem();
     }
 }
