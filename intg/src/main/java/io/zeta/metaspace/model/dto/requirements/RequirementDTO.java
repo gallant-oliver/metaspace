0b9f7a4163c732b9173f8e4518a13dd012ff2c02
周磊 <zhoulei@gridsum.com>
2021-12-07 18:03:26 +0800

    [feature/需求管理] 新增需求管理DTO以及枚举

---
+++ intg/src/main/java/io/zeta/metaspace/model/dto/requirements/RequirementDTO.java
@@ -0,0 +1,64 @@
+package io.zeta.metaspace.model.dto.requirements;
+
+import io.zeta.metaspace.model.enums.ApiProtocol;
+import io.zeta.metaspace.model.enums.ResourceType;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * 需求管理 - 需求
+ *
+ * @author 周磊
+ * @version 1.0
+ * @date 2021-12-07
+ */
+@Data
+public class RequirementDTO {
+    private String guid;
+    /**
+     * 需求名称
+     */
+    private String name;
+    /**
+     * 需求编码
+     */
+    private String num;
+    /**
+     * 资源类型 {@link ResourceType#getDesc()}
+     */
+    private ResourceType resourceType;
+    private String version;
+    /**
+     * {@code this.resourceType == ResourceType.API } 时字段有效;
+     * <p>
+     * 参数协议 {@link ApiProtocol#name()}
+     */
+    private String agreement;
+    /**
+     * {@code this.resourceType == ResourceType.API } 时字段有效;
+     * <p>
+     * 请求方式: GET/POST
+     */
+    private String requestMode;
+    /**
+     * 目标字段ID
+     */
+    private List<String> targetFieldIDs;
+    /**
+     * 过滤字段
+     */
+    private List<FilterConditionDTO> FilterConditions;
+    /**
+     * 文件原始名称
+     */
+    private String fileName;
+    /**
+     * HDFS服务器文件存储路径
+     */
+    private String filePath;
+    /**
+     * 描述
+     */
+    private String description;
+}
---
+++ intg/src/main/java/io/zeta/metaspace/model/dto/requirements/ResourceDTO.java
@@ -0,0 +1,45 @@
+package io.zeta.metaspace.model.dto.requirements;
+
+import io.zeta.metaspace.model.enums.ResourceState;
+import io.zeta.metaspace.model.enums.ResourceType;
+import lombok.Data;
+
+/**
+ * 需求管理 - 资源
+ *
+ * @author 周磊
+ * @version 1.0
+ * @date 2021-12-07
+ */
+@Data
+public class ResourceDTO {
+    /**
+     * 资源所属的需求ID
+     */
+    private String requirementId;
+    /**
+     * 资源名称
+     */
+    private String name;
+    /**
+     * 资源类型 {@link ResourceType#getDesc()}
+     */
+    private ResourceType type;
+    /**
+     * 数据表名
+     */
+    private String dataTableName;
+    /**
+     * 版本
+     */
+    private String version;
+    /**
+     * 状态 {@link ResourceState#getDesc()}
+     */
+    private ResourceState state;
+    /**
+     * 创建人账号名称
+     */
+    private String creator;
+    
+}
