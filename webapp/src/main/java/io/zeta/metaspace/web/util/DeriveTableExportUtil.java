25525fd9d890ff300bb59f3b31255537eaf62509
DESKTOP-AQVNHCJ\Gridsum <huangrongwen@gridsum.com>
2022-01-11 12:00:11 +0800

    refactor:异常的错误抓取，以及一些格式做调整

--- webapp/src/main/java/io/zeta/metaspace/web/util/DeriveTableExportUtil.java
+++ webapp/src/main/java/io/zeta/metaspace/web/util/DeriveTableExportUtil.java
@@ -5,7 +5,6 @@ import io.zeta.metaspace.model.sourceinfo.derivetable.vo.SourceInfoDeriveColumnV
 import lombok.Data;
 import org.apache.atlas.exception.AtlasBaseException;
 import org.springframework.beans.BeanUtils;
-import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -23,6 +22,7 @@ public class DeriveTableExportUtil {
     private final static String DERIVE_TABLE_EXPORT_FILE_NAME = "deriveTableExport.xlsx";
     private final static String DERIVE_IMPORT_TEMPLATE = "衍生表批量登记模板_.xlsx";
     private final static String SPACE = "\\conf\\";
+
     public static String getDeriveImportTemplate() {
         return DERIVE_IMPORT_TEMPLATE;
     }
@@ -31,62 +31,60 @@ public class DeriveTableExportUtil {
      * 获取导出衍生表的模板名字
      * @return
      */
-    public static String deriveTableTemplate(){
-        return FILE_PATH+SPACE+DERIVE_TABLE_EXPORT_FILE_NAME;
+    public static String deriveTableTemplate() {
+        return FILE_PATH + SPACE + DERIVE_TABLE_EXPORT_FILE_NAME;
     }
 
     /**
      * 获取衍生表导入模板
      * @return
      */
-    public static File deriveTableImportTemplate(){
-        File resource = new File(FILE_PATH+SPACE+DERIVE_IMPORT_TEMPLATE);
-        if(!resource.exists()){
+    public static File deriveTableImportTemplate() {
+        File resource = new File(FILE_PATH + SPACE + DERIVE_IMPORT_TEMPLATE);
+        if (!resource.exists()) {
             throw new AtlasBaseException("找不到导出模板表");
         }
         return resource;
     }
 
     /**
-     *
      * @return
      */
-    public static File deriveTableExport(String tableName){
-        File classPathResource = new File(tableName);
-        if(!classPathResource.exists()){
+    public static File deriveTableExport(String tableName) {
+        File file = new File(tableName);
+        if (!file.exists()) {
             throw new AtlasBaseException("生成衍生表excel失败");
         }
-        return classPathResource;
+        return file;
     }
 
     /**
-     *
      * @return
      */
-    public static String deriveTableExcelName(String tableName){
-        return FILE_PATH+SPACE+"衍生表_"+tableName+".xlsx";
+    public static String deriveTableExcelName(String tableName) {
+        return FILE_PATH + SPACE + "衍生表_" + tableName + ".xlsx";
     }
 
-    public static List<SourceInfoDeriveColumnDTO> getPojo(List<SourceInfoDeriveColumnVO> sourceInfoDeriveColumnVOS){
+    public static List<SourceInfoDeriveColumnDTO> getPojo(List<SourceInfoDeriveColumnVO> sourceInfoDeriveColumnVOS) {
         List<SourceInfoDeriveColumnDTO> list = new ArrayList<>();
-        for (SourceInfoDeriveColumnVO vo1:sourceInfoDeriveColumnVOS) {
+        for (SourceInfoDeriveColumnVO vo1 : sourceInfoDeriveColumnVOS) {
             SourceInfoDeriveColumnDTO sourceInfoDeriveColumnDTO = new SourceInfoDeriveColumnDTO();
-            BeanUtils.copyProperties(vo1,sourceInfoDeriveColumnDTO);
-            sourceInfoDeriveColumnDTO.setGroupField(getTureOrFalse(vo1.isGroupField()));
-            sourceInfoDeriveColumnDTO.setRemoveSensitive(getTureOrFalse(vo1.isRemoveSensitive()));
-            sourceInfoDeriveColumnDTO.setImportant(getTureOrFalse(vo1.isImportant()));
-            sourceInfoDeriveColumnDTO.setPermissionField(getTureOrFalse(vo1.isPermissionField()));
-            sourceInfoDeriveColumnDTO.setSecret(getTureOrFalse(vo1.isSecret()));
-            sourceInfoDeriveColumnDTO.setPrimaryKey(getTureOrFalse(vo1.isPrimaryKey()));
+            BeanUtils.copyProperties(vo1, sourceInfoDeriveColumnDTO);
+            sourceInfoDeriveColumnDTO.setGroupField(getTrueOrFalse(vo1.isGroupField()));
+            sourceInfoDeriveColumnDTO.setRemoveSensitive(getTrueOrFalse(vo1.isRemoveSensitive()));
+            sourceInfoDeriveColumnDTO.setImportant(getTrueOrFalse(vo1.isImportant()));
+            sourceInfoDeriveColumnDTO.setPermissionField(getTrueOrFalse(vo1.isPermissionField()));
+            sourceInfoDeriveColumnDTO.setSecret(getTrueOrFalse(vo1.isSecret()));
+            sourceInfoDeriveColumnDTO.setPrimaryKey(getTrueOrFalse(vo1.isPrimaryKey()));
             list.add(sourceInfoDeriveColumnDTO);
         }
         return list;
     }
 
-    private static String getTureOrFalse(boolean f){
-        if(f){
+    private static String getTrueOrFalse(boolean f) {
+        if (f) {
             return "是";
-        }else {
+        } else {
             return "否";
         }
     }
