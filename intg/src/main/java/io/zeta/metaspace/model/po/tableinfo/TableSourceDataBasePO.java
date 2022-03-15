4b7333b012dbc15af9bf1c033e11f7a796b728ac
wuhui <wuhui@gridsum.com>
2022-03-15 17:43:02 +0800

    数据血缘（老接口hive添加）

---
+++ intg/src/main/java/io/zeta/metaspace/model/po/tableinfo/TableSourceDataBasePO.java
@@ -0,0 +1,35 @@
+package io.zeta.metaspace.model.po.tableinfo;
+
+import lombok.Data;
+
+/**
+ * 源数据配置信息
+ * @author w
+ */
+@Data
+public class TableSourceDataBasePO {
+    /**
+     * 数据表ID
+     */
+    private String guid;
+    /**
+     * 查询的数据库ip
+     */
+    private String host;
+    /**
+     * 查询的数据库端口
+     */
+    private Integer port;
+    /**
+     * 查询的数据库名称
+     */
+    private String database;
+    /**
+     * 查询的数据表
+     */
+    private String table;
+    /**
+     * 查询的数据库类型（Mysql、Oracle）
+     */
+    private String type;
+}
