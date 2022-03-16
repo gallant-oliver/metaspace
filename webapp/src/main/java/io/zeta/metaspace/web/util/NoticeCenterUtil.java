4c00237ecee8127418640162db98f150041950b9
haobiao <haobiao@gridsum.com>
2022-03-16 14:52:34 +0800

    元数据变更通知

--- webapp/src/main/java/io/zeta/metaspace/web/util/NoticeCenterUtil.java
+++ webapp/src/main/java/io/zeta/metaspace/web/util/NoticeCenterUtil.java
@@ -25,11 +25,11 @@ public class NoticeCenterUtil {
     //定义邮件通知的接口
     private final static String NOTICE_EMAIL_API = "/notice-center/api/v1/missions";
     //定义站内信
-    private final static String SEND_MESSAGE_API="/securecenter/api/v1/message/AddOneMessage";
+    private final static String SEND_MESSAGE_API = "/securecenter/api/v1/message/AddOneMessage";
     //站内信response放回状态码
     private final static double MESSAGE_SEND_SUCCESS_CODE = 0.0;
 
-    public static boolean sendMessage(String message, String title,String userName) {
+    public static boolean sendMessage(String message, String title, String userName) {
         String url = "";
         try {
             Configuration configuration = ApplicationProperties.get();
@@ -43,11 +43,11 @@ public class NoticeCenterUtil {
             return false;
         }
         log.info("组装发送站内信请求参数...");
-        HashMap<String, Object> headerMap = new HashMap<String, Object>(1) ;
+        HashMap<String, Object> headerMap = new HashMap<>();
         //站内信白名单设置，不传token，只传-1的tenantId
         headerMap.put("tenantId", "-1");
         log.info("组装发送站内信请求body.");
-        HashMap<String, Object> jsonMap = new HashMap<>(3);
+        HashMap<String, Object> jsonMap = new HashMap<>();
         jsonMap.put("loginEmail", userName);
         jsonMap.put("message", message);
         jsonMap.put("title", title);
@@ -59,70 +59,72 @@ public class NoticeCenterUtil {
         }
         Gson gson = new Gson();
         Map map = gson.fromJson(response, HashMap.class);
-        double errorCode = (Double)map.get("errorCode");
-        if (MESSAGE_SEND_SUCCESS_CODE==errorCode) {
+        double errorCode = (Double) map.get("errorCode");
+        if (MESSAGE_SEND_SUCCESS_CODE == errorCode) {
             log.info("发送站内信成功");
             return true;
         }
         return false;
     }
 
-    public static boolean sendEmail(String attachmentBase64content,String fileName,String content,String[] contacts){
+    public static boolean sendEmail(String attachmentBase64content, String fileName, String content, String[] contacts) {
         String emailUrlPrefix = "";
         try {
-            emailUrlPrefix =  ApplicationProperties.get().getString("notice.email.url");
+            emailUrlPrefix = ApplicationProperties.get().getString("notice.email.url");
         } catch (AtlasException e) {
             log.error("获取邮件通知url前缀失败", e);
             throw new AtlasBaseException(e);
         }
 
-        if(StringUtils.isBlank(emailUrlPrefix)){
+        if (StringUtils.isBlank(emailUrlPrefix)) {
             log.error("properties ENV param [notice.email.url] is not setting");
             return false;
         }
         log.info("组装发送邮件请求参数...");
         //组装请求接口参数
-        HashMap<String,Object> headerMap =  new HashMap<String, Object>(3){
+        HashMap<String, Object> headerMap = new HashMap<String, Object>(3) {
             private static final long serialVersionUID = 1L;
+
             {
-                put("Content-Type","application/json");
+                put("Content-Type", "application/json");
             }
         };
-        Map<String,Object> queryParamMap = new HashMap<String, Object>(1){
+        Map<String, Object> queryParamMap = new HashMap<String, Object>(1) {
             private static final long serialVersionUID = 10L;
+
             {
-                put("cmd","CreateMessageMission");
+                put("cmd", "CreateMessageMission");
             }
         };
         log.info("组装发送邮件请求body.");
-        HashMap<String,Object> jsonMap = new HashMap<>();
-        jsonMap.put("event_id","notice__"+ UUIDUtils.alphaUUID());
-        jsonMap.put("channel_instance_id",3);
+        HashMap<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put("event_id", "notice__" + UUIDUtils.alphaUUID());
+        jsonMap.put("channel_instance_id", 3);
         jsonMap.put("priority", 0);
         jsonMap.put("template_id", 0);
         //邮件地址列表去重
         List<String> contactList = Stream.of(contacts).distinct().collect(Collectors.toList());
         contacts = contactList.toArray(new String[contactList.size()]);
         jsonMap.put("contacts", contacts);
-        jsonMap.put("datas", new HashMap<String,String>(1){{
-            put("content",content);
+        jsonMap.put("datas", new HashMap<String, String>(1) {{
+            put("content", content);
         }});
-        jsonMap.put("attachment", new HashMap<String,String>(2){{
-            put("payload",attachmentBase64content);
-            put("file_name",fileName);
+        jsonMap.put("attachment", new HashMap<String, String>(2) {{
+            put("payload", attachmentBase64content);
+            put("file_name", fileName);
         }});
         String json = new JSONObject(jsonMap).toString();
         log.info("执行发送邮件请求命令.");
-        String responseStr = OKHttpClient.doPost(emailUrlPrefix+NOTICE_EMAIL_API,headerMap,queryParamMap,json,3);
-        log.info("通知返回响应:: {}",responseStr);
-        if(StringUtils.isBlank(responseStr)){
+        String responseStr = OKHttpClient.doPost(emailUrlPrefix + NOTICE_EMAIL_API, headerMap, queryParamMap, json, 3);
+        log.info("通知返回响应:: {}", responseStr);
+        if (StringUtils.isBlank(responseStr)) {
             log.error("请求email服务失败");
             return false;
         }
         Gson gson = new Gson();
         Map map = gson.fromJson(responseStr, HashMap.class);
-        Object status =  map.getOrDefault("error_code","-1");
-        if( "0".equals(status) ){
+        Object status = map.getOrDefault("error_code", "-1");
+        if ("0".equals(status)) {
             log.info("发送email成功");
             return true;
         }
