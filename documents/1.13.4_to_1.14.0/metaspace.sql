3cdfb39c1d39642391884d11fbfc97b714734c69
haobiao <haobiao@gridsum.com>
2022-04-13 13:55:52 +0800

    完善脚本

--- documents/1.13.4_to_1.14.0/metaspace.sql
+++ documents/1.13.4_to_1.14.0/metaspace.sql
@@ -9,3 +9,17 @@ WHERE t1.column_guid = t2.column_guid AND sort is null;
 
 -- 创建data_quality_task_rule_execute表，创建时间字段索引（用于数据管理的告警列表的筛选）
 CREATE INDEX data_quality_task_rule_execute_create_time_idx ON public.data_quality_task_rule_execute (create_time);
+
+-- 指标图书馆
+
+ALTER TABLE public.indicator_threshold_log ADD actual_value varchar(32) NULL;
+COMMENT ON COLUMN public.indicator_threshold_log.actual_value IS '实际值';
+
+ALTER TABLE public.indicator_threshold_log ADD threshold_setting_id int8 NULL;
+COMMENT ON COLUMN public.indicator_threshold_log.threshold_setting_id IS '阈值设置id';
+
+ALTER TABLE "public"."business_indicators" add "operations_people_ids" text[] COLLATE "pg_catalog"."default";
+COMMENT ON COLUMN "public"."business_indicators"."operations_people_ids" IS '运维负责人ID列表';
+
+
+
