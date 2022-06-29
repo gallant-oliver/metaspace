30736871bde6d2faaea1903262462af7fe27ebf3
liwenfeng <liwenfeng@gridsum.com>
2022-06-29 10:20:19 +0800

    修改sql注入问题

--- webapp/src/main/java/io/zeta/metaspace/web/dao/DataSourceDAO.java
+++ webapp/src/main/java/io/zeta/metaspace/web/dao/DataSourceDAO.java
@@ -170,10 +170,33 @@ public interface DataSourceDAO {
             "and us.username like concat('%',#{dataSourceSearch.updateUserName},'%') ESCAPE '/'" +
             "</if>" +
             "<if test='parameters.sortby!=null'>" +
-            "order by ds.#{parameters.sortby} " +
+            "<choose>" +
+            "<when test='parameters.sortby==&quot;source_name&quot;'>" +
+            "order by ds.source_name " +
+            "</when>" +
+            "<when test='parameters.sortby==&quot;sourceType&quot;'>" +
+            "order by ds.source_type " +
+            "</when>" +
+            "<when test='parameters.sortby==&quot;createTime&quot;'>" +
+            "order by ds.create_time " +
+            "</when>" +
+            "<when test='parameters.sortby==&quot;updateTime&quot;'>" +
+            "order by ds.update_time " +
+            "</when>" +
+            "</choose>" +
+            "</if>" +
+            "<if test='parameters.sortby==null'>" +
+            "order by ds.source_name " +
             "</if>" +
             "<if test='parameters.order!=null and parameters.sortby!=null'>" +
-            "#{parameters.order} " +
+            "<choose>" +
+            "<when test='parameters.order==&quot;desc&quot;'>" +
+            "desc " +
+            "</when>" +
+            "<otherwise>" +
+            "asc " +
+            "</otherwise>" +
+            "</choose>" +
             "</if>" +
             "<if test='parameters.limit!=-1'>" +
             "limit #{parameters.limit} " +
@@ -250,10 +273,33 @@ public interface DataSourceDAO {
             "and us.username like concat('%',#{dataSourceSearch.updateUserName},'%') ESCAPE '/'" +
             "</if>" +
             "<if test='parameters.sortby!=null'>" +
-            "order by ds.#{parameters.sortby} " +
+            "<choose>" +
+            "<when test='parameters.sortby==&quot;source_name&quot;'>" +
+            "order by ds.source_name " +
+            "</when>" +
+            "<when test='parameters.sortby==&quot;sourceType&quot;'>" +
+            "order by ds.source_type " +
+            "</when>" +
+            "<when test='parameters.sortby==&quot;createTime&quot;'>" +
+            "order by ds.create_time " +
+            "</when>" +
+            "<when test='parameters.sortby==&quot;updateTime&quot;'>" +
+            "order by ds.update_time " +
+            "</when>" +
+            "</choose>" +
+            "</if>" +
+            "<if test='parameters.sortby==null'>" +
+            "order by ds.source_name " +
             "</if>" +
             "<if test='parameters.order!=null and parameters.sortby!=null'>" +
-            "#{parameters.order} " +
+            "<choose>" +
+            "<when test='parameters.order==&quot;desc&quot;'>" +
+            "desc " +
+            "</when>" +
+            "<otherwise>" +
+            "asc " +
+            "</otherwise>" +
+            "</choose>" +
             "</if>" +
             "<if test='parameters.limit!=-1'>" +
             "limit #{parameters.limit} " +
