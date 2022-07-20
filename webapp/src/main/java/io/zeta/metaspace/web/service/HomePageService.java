dcfd1474444e51130514b3fdbf1cd00fed328309
haobiao <haobiao@gridsum.com>
2022-07-20 14:38:24 +0800

    修改概览页面接口

--- webapp/src/main/java/io/zeta/metaspace/web/service/HomePageService.java
+++ webapp/src/main/java/io/zeta/metaspace/web/service/HomePageService.java
@@ -24,7 +24,6 @@ import io.zeta.metaspace.model.metadata.Parameters;
 import io.zeta.metaspace.model.result.CategoryPrivilege;
 import io.zeta.metaspace.model.result.PageResult;
 import io.zeta.metaspace.web.dao.*;
-import io.zeta.metaspace.web.util.AdminUtils;
 import io.zeta.metaspace.web.util.DateUtils;
 import io.zeta.metaspace.web.util.ReturnUtil;
 import org.apache.atlas.AtlasErrorCode;
@@ -485,6 +484,9 @@ public class HomePageService {
         HomeTaskInfo taskInfo = null;
         try {
             taskInfo = homePageDAO.getTaskInfo(tenantId);
+            if (taskInfo == null) {
+                taskInfo = new HomeTaskInfo();
+            }
         } catch (Exception e) {
             throw new AtlasBaseException(AtlasErrorCode.BAD_REQUEST, "任务详情查询异常");
         }
