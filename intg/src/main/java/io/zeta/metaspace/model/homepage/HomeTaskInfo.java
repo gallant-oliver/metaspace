1d5c867674a0845c0a44b3b244fb75ca113209b6
haobiao <haobiao@gridsum.com>
2022-07-25 14:22:21 +0800

    解决代码扫描问题

--- intg/src/main/java/io/zeta/metaspace/model/homepage/HomeTaskInfo.java
+++ intg/src/main/java/io/zeta/metaspace/model/homepage/HomeTaskInfo.java
@@ -2,13 +2,16 @@ package io.zeta.metaspace.model.homepage;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @author huangrongwen
  * @Description: 概览-质量任务详情
  * @date 2022/3/910:55
  */
 @Data
-public class HomeTaskInfo {
+public class HomeTaskInfo implements Serializable {
+    private static final long serialVersionUID = -7220348770322246885L;
     private long redCount;
     private long orangeCount;
     private long generalCount;
