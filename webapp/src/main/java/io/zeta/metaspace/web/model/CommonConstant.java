adecdd6031114d23f2b612da560cc944ece506a9
黄荣文 <huangrongwen@gridsum.com>
2022-08-26 18:23:25 +0800

    数据服务对接apisix

--- webapp/src/main/java/io/zeta/metaspace/web/model/CommonConstant.java
+++ webapp/src/main/java/io/zeta/metaspace/web/model/CommonConstant.java
@@ -161,4 +161,11 @@ public class CommonConstant {
     public static final String PUBLIC = "PUBLIC";
 
     public static final String ASSET_NAME = "Asset.name";
+
+    private static final String API_SIX_CREATE_PATH = "/apisix/admin/routes";
+
+    public static final int MOBIUS_TYPE = 1;
+    public static final int API_SIX_TYPE = 2;
+
+    public static final String API_ACCESS_PREFIX = "/api/metaspace/dataservice/";
 }
