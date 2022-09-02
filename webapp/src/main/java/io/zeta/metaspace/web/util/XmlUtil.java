8e04d9d0c27c2589bbf3ce430002b963edcae9a8
黄荣文 <huangrongwen@gridsum.com>
2022-09-02 15:07:43 +0800

    添加默认参数

--- webapp/src/main/java/io/zeta/metaspace/web/util/XmlUtil.java
+++ webapp/src/main/java/io/zeta/metaspace/web/util/XmlUtil.java
@@ -6,11 +6,11 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import java.io.StringWriter;
 
+import org.apache.atlas.exception.AtlasBaseException;
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.io.OutputFormat;
 import org.dom4j.io.XMLWriter;
-
 /**
  * @author huangrongwen
  * @Description: xml工具类
@@ -36,18 +36,16 @@ public class XmlUtil {
         try {
             return formatXml(getXmlMapper().writeValueAsString(value));
         }catch (Exception e) {
-            e.printStackTrace();
+            throw new AtlasBaseException("xml格式转换失败",e);
         }
-        return null;
     }
 
     public static String toXml(Object value) {
         try {
             return getXmlMapper().writeValueAsString(value);
         }catch (Exception e) {
-            e.printStackTrace();
+            throw new AtlasBaseException("xml格式转换失败",e);
         }
-        return null;
     }
 
 
